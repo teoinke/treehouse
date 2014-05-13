44931e5d0e6ad7b16216ce7bd610549fac689be8
Teo Umanni <teoinke@hotmail.com>
2014-05-13 11:27:48 -0300

    First front-end development phase 1

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Treebook::Application.routes.draw do
 
   devise_for :users
   resources :statuses
-  root to: 'statuses#index'
+  root to: 'quedas#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
